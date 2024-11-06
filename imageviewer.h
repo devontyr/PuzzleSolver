d703335a6ab378bbe85bdacca43a3e9c53ce8ca7
AJPez24 <anthony.j.pezza@gmail.com>
2024-11-06 10:48:22 -0500

    Add zoom

--- imageviewer.h
+++ imageviewer.h
@@ -1,17 +1,29 @@
 #ifndef IMAGEVIEWER_H
 #define IMAGEVIEWER_H
+
 #include <QGraphicsView>
 
 class ImageViewer : public QGraphicsView
 {
     Q_OBJECT
+
     QGraphicsScene scene;
     QImage image;
     QGraphicsPixmapItem *giPixmap;
     double scale;
 
 public:
+
     ImageViewer(const QImage &_image);
+
+signals:
+
+    void mouseMoved(QPoint pos, QColor color);
+
+protected:
+
+    void keyPressEvent(QKeyEvent *evt) override;
+    void mouseMoveEvent(QMouseEvent *evt) override;
 };
 
 #endif // IMAGEVIEWER_H
