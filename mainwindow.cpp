1b8dfc77bb2cd35d149b2da7bfee0f10c39aba2a
Anthony Pezza <159830214+AJPez24@users.noreply.github.com>
2024-11-13 15:47:33 -0500

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,8 +1,9 @@
 #include "mainwindow.h"
 #include <imageviewer.h>
 #include <QtWidgets>
+#include <QtMath>
 
-MainWindow *mainWindow;
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent) {
@@ -36,41 +37,136 @@ MainWindow::~MainWindow() {
 }
 
 bool MainWindow::isShadeOfWhite(const QRgb &color) {
-    int threshold = 10; // larger num if we want to accept more gray colors as "white"
-    int minBrightness = 169; // smaller num allows more gray colors as "white"
-
     //values are between 0-255
     int r = qRed(color);
     int g = qGreen(color);
     int b = qBlue(color);
 
-    // Check if r/g/b are right hue (pure white/black would be 0)
-    if (abs(r - g) > threshold || abs(r - b) > threshold || abs(g - b) > threshold) {
+    int combo = r + g + b;
+
+    if (combo <= 600 && combo >=200){
+        if ((qAbs(b-r) + qAbs(b-g) +qAbs(r-g) > 28) && (b > r || b > g)){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    else{
+        return true;
+    }
+}
+
+bool MainWindow::isSurroundedRed(int pixelX, int pixelY){
+    int redCount = 0;
+    if ((pixelX == 0) || (pixelY == 0) || (pixelX == processedImage->width() - 1) || (pixelY == processedImage->height() - 1)){
         return false;
     }
-    // Check if r/g/b is right shade of white-gray-black
-    return r >= minBrightness && g >= minBrightness && b >= minBrightness;
-}
 
-int Waiter::count = 0;
+    QColor pixel1 = processedImage->pixel(pixelX - 1, pixelY - 1);
+    QColor pixel2 = processedImage->pixel(pixelX, pixelY - 1);
+    QColor pixel3 = processedImage->pixel(pixelX + 1, pixelY - 1);
 
-Waiter::Waiter() {
-    if (++count == 1) {
-        mainWindow->setEnabled(false);
-        mainWindow->statusBar()->showMessage("patience is a virtue...");
-        mainWindow->statusBar()->show();
-        QApplication::processEvents();
+    QColor pixel4 = processedImage->pixel(pixelX -1, pixelY);
+    QColor pixel5 = processedImage->pixel(pixelX + 1, pixelY );
+
+    QColor pixel6 = processedImage->pixel(pixelX - 1, pixelY + 1);
+    QColor pixel7 = processedImage->pixel(pixelX, pixelY + 1);
+    QColor pixel8 = processedImage->pixel(pixelX + 1, pixelY + 1);
+
+
+    if (pixel1.red() == 255){
+        ++redCount;
     }
+
+    if (pixel2.red() == 255){
+        ++redCount;
+    }
+
+    if (pixel3.red() == 255){
+        ++redCount;
+    }
+
+    if (pixel4.red() == 255){
+        ++redCount;
+    }
+
+    if (pixel5.red() == 255){
+        ++redCount;
+    }
+
+    if (pixel6.red() == 255){
+        ++redCount;
+    }
+
+    if (pixel7.red() == 255){
+        ++redCount;
+    }
+
+    if (pixel8.red() == 255){
+        ++redCount;
+    }
+
+    return (redCount >= 5);
 }
 
-Waiter::~Waiter(){
-    if (--count == 0) {
-        mainWindow->setEnabled(true);
-        mainWindow->statusBar()->clearMessage();
-        mainWindow->statusBar()->hide();
-        QApplication::processEvents();
+
+bool MainWindow::isSurroundedBlack(int pixelX, int pixelY){
+    int blackCount = 0;
+    if ((pixelX == 0) || (pixelY == 0) || (pixelX == processedImage->width() - 1) || (pixelY == processedImage->height() - 1)){
+        return false;
     }
+
+    QColor pixel1 = processedImage->pixel(pixelX - 1, pixelY - 1);
+    QColor pixel2 = processedImage->pixel(pixelX, pixelY - 1);
+    QColor pixel3 = processedImage->pixel(pixelX + 1, pixelY - 1);
+
+    QColor pixel4 = processedImage->pixel(pixelX -1, pixelY);
+    QColor pixel5 = processedImage->pixel(pixelX + 1, pixelY );
+
+    QColor pixel6 = processedImage->pixel(pixelX - 1, pixelY + 1);
+    QColor pixel7 = processedImage->pixel(pixelX, pixelY + 1);
+    QColor pixel8 = processedImage->pixel(pixelX + 1, pixelY + 1);
+
+
+    if (pixel1.red() == 0){
+        ++blackCount;
+    }
+
+    if (pixel2.red() == 0){
+        ++blackCount;
+    }
+
+    if (pixel3.red() == 0){
+        ++blackCount;
+    }
+
+    if (pixel4.red() == 0){
+        ++blackCount;
+    }
+
+    if (pixel5.red() == 0){
+        ++blackCount;
+    }
+
+    if (pixel6.red() == 0){
+        ++blackCount;
+    }
+
+    if (pixel7.red() == 0){
+        ++blackCount;
+    }
+
+    if (pixel8.red() == 0){
+        ++blackCount;
+    }
+
+    return (blackCount >= 6);
 }
+
+
+
+
 
 void MainWindow::openImageSlot() {
     //open file as QImage and put on screen
@@ -95,10 +191,38 @@ void MainWindow::openImageSlot() {
             if (!isShadeOfWhite(pixel)) {
                 processedImage->setPixelColor(QPoint(x,y), Qt::red); //mark the pixel as a puzzle piece
             }
+            else{
+                processedImage->setPixelColor(QPoint(x,y), Qt::black); //mark the pixel as a puzzle piece
+
+            }
         }
     }
+
+    for (int y = 0; y < image.height(); ++y) {
+        for (int x = 0; x < image.width(); ++x) {
+
+            if(isSurroundedBlack(x,y)){
+                processedImage->setPixelColor(QPoint(x,y), Qt::black); //mark the pixel as red
+            }
+            if(isSurroundedRed(x, y)){
+                processedImage->setPixelColor(QPoint(x,y), Qt::red); //mark the pixel as red
+            }
+        }
+    }
+
+    for (int y = 0; y < image.height(); ++y) {
+        for (int x = 0; x < image.width(); ++x) {
+
+            if(isSurroundedBlack(x,y)){
+                processedImage->setPixelColor(QPoint(x,y), Qt::black); //mark the pixel as red
+            }
+            if(isSurroundedRed(x, y)){
+                processedImage->setPixelColor(QPoint(x,y), Qt::red); //mark the pixel as red
+            }
+        }
+    }
+
+
     puzzleLayout = new PuzzleSolverLayout(*processedImage);
     setCentralWidget(puzzleLayout);
-
-    setCentralWidget(interactivePieceLayout);
 }
